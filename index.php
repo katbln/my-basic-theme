c36b46f3d347a7c35007943743f7319eec1e5aa0
nouvell <kat_bln@hotmail.de>
2020-12-02 14:59:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,16 +12,9 @@
     <?php while( have_posts() ) : the_post(); ?>
     <article>
         <h3><?php the_title(); ?></h3>
-        <?php the_content (); ?>
+        <?php the_content(); ?>
     </article>
     <?php endwhile; ?>
-
-</body>
-
-</html>
-
-
-
 
 </body>
 
