c5b562a23034236b81983b8f274307f1d36376d1
nouvell <kat_bln@hotmail.de>
2020-12-02 14:57:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title><?php bloginfo('title'); ?></title>
-     <?php wp_head() ?>
+    <?php wp_head() ?>
 </head>
 
 <body>
     <h1><?php bloginfo('title'); ?></h1>
+
+    <?php while( have_posts() ) : the_post(); ?>
+    <article>
+        <h3><?php the_title(); ?></h3>
+        <?php the_content (); ?>
+    </article>
+    <?php endwhile; ?>
+
+</body>
+
+</html>
+
+
+
+
 </body>
 
 </html>
