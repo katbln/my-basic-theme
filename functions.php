90fdc7fbe7f695e46f8be02342a37e65328c009f
nouvell <kat_bln@hotmail.de>
2020-12-02 15:24:04 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,3 +4,14 @@ function my_style() {
     wp_enqueue_style(  'style', get_stylesheet_uri()); 
 }
 add_action( 'wp_enqueue_scripts', 'my_style' );
+
+function register_my_menus() {
+  register_nav_menus(
+    array(
+        'header-menu' => __( 'Header Menu' ),
+        'mobile-menu' => __( 'Mobil Menu' ),
+        'footer-menu' => __( 'Footer Menu' )
+     )
+   );
+ }
+ add_action( 'init', 'register_my_menus' );
