44642c58ea420e7a1d1ccd754ba78096850bb4c6
nouvell <kat_bln@hotmail.de>
2020-12-02 15:27:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,13 @@
 </head>
 
 <body>
+    
+    <nav>
+    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+    </nav>
     <h1><?php bloginfo('title'); ?></h1>
-        <?php while( have_posts() ) : the_post(); ?>
+
+    <?php while( have_posts() ) : the_post(); ?>
     <article>
         <h3><?php the_title(); ?></h3>
         <?php the_content(); ?>
