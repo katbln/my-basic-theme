1a5f8f980b047c82532908e628f43b0262056322
katbln <kat_bln@hotmail.de>
2020-12-15 15:41:35 +0100

    .

--- modul-a1.php
+++ modul-a1.php
@@ -4,12 +4,19 @@ Template Name: Modul A
 */
 ?>
 <?php get_header(); ?>
+<body>
+    <div class="oben-bg"></div>
+    <div class="mitte-bg">
+    <article>
+        <h2>Überschrift</h2>
+        <p>Hier steht Text.</p>
+        <p>
 
-<h3>Hier sehen Sie ein Übersicht der einzelnen Module in der Kategorie A</h3>
+            Es gibt im Moment in diese Mannschaft, oh, einige Spieler vergessen ihnen Profi was sie sind. Ich lese nicht sehr viele Zeitungen, aber ich habe gehört viele Situationen. Erstens: wir haben nicht offensiv gespielt. Es gibt keine deutsche Mannschaft spielt offensiv und die Name offensiv wie Bayern. Letzte Spiel hatten wir in Platz drei Spitzen: Elber, Jancka und dann Zickler. Wir müssen nicht vergessen Zickler. Zickler ist eine Spitzen mehr, Mehmet eh mehr Basler. Ist klar diese Wörter, ist möglich verstehen, was ich hab gesagt? Danke. Offensiv, offensiv ist wie machen wir in Platz. Zweitens: ich habe erklärt mit diese zwei Spieler: nach Dortmund brauchen vielleicht Halbzeit Pause. Ich habe auch andere Mannschaften gesehen in Europa nach diese Mittwoch. Ich habe gesehen auch zwei Tage die Training. Ein Trainer ist nicht ein Idiot! Ein Trainer sei sehen was passieren in Platz. In diese Spiel es waren zwei, drei diese Spieler waren schwach wie eine Flasche leer! Haben Sie gesehen Mittwoch, welche Mannschaft hat gespielt Mittwoch? Hat gespielt Mehmet oder gespielt Basler oder hat gespielt Trapattoni? </p>
+    </article>
+    </div>
 
-   <section class="modula">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/bg-a.png" width=100% alt="Hintergrund Grafi in Rosa">
-</section>
+    <div class="unten-bg"></div>
+</body>
 
-
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
