e3c63f8290271b89c9d91b28474d7bdc3f4e8001
katbln <kat_bln@hotmail.de>
2020-12-16 10:39:03 +0100

    .

--- modul-a1.php
+++ modul-a1.php
@@ -5,18 +5,26 @@ Template Name: Modul A
 ?>
 <?php get_header(); ?>
 <body>
-    <div class="oben-bg"></div>
-    <div class="mitte-bg">
-    <div class="logo-wrapper">
-            <img src="img/auge.svg" with="100%">
+
+    <div class="container above-content"></div>
+    <div class="container content-wrapper">
+        <div class="logo-wrapper">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/auge.svg" with="100%">
         </div>
-    <article>
-        <h2>Überschrift</h2>
-        <p>Hier steht Text.</p>
-        <p>
-            Es gibt im Moment in diese Mannschaft, oh, einige Spieler vergessen ihnen Profi was sie sind. Ich lese nicht sehr viele Zeitungen, aber ich habe gehört viele Situationen. Erstens: wir haben nicht offensiv gespielt. Es gibt keine deutsche Mannschaft spielt offensiv und die Name offensiv wie Bayern. Letzte Spiel hatten wir in Platz drei Spitzen: Elber, Jancka und dann Zickler. Wir müssen nicht vergessen Zickler. Zickler ist eine Spitzen mehr, Mehmet eh mehr Basler. Ist klar diese Wörter, ist möglich verstehen, was ich hab gesagt? </p>
-    </article>
+        
+        <div class="kasten-logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/kasten-logo.svg" with="100%">
+        </div>
+        <article>
+
+            <h2>Überschrift</h2>
+            <p>Hier steht Text.</p>
+            <p>
+                Es gibt im Moment in diese Mannschaft, oh, einige Spieler vergessen ihnen Profi was sie sind. Ich lese nicht sehr viele Zeitungen, aber ich habe gehört viele Situationen. Erstens: wir haben nicht offensiv gespielt. Es gibt keine deutsche Mannschaft spielt offensiv und die Name offensiv wie Bayern. Letzte Spiel hatten wir in Platz drei Spitzen: Elber, Jancka und dann Zickler. Wir 
+                </p>
+        </article>
     </div>
-    <div class="unten-bg"></div>
+    <div class="container below-content"></div>
+
 </body>
     <?php get_footer(); ?>
