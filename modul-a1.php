4994a242c6ae909cc0250ea2b3ec4c9547be8769
katbln <kat_bln@hotmail.de>
2020-12-17 09:06:35 +0100

    .

--- modul-a1.php
+++ modul-a1.php
@@ -4,27 +4,32 @@ Template Name: Modul A
 */
 ?>
 <?php get_header(); ?>
-<body>
 
+<main>
     <div class="container above-content"></div>
     <div class="container content-wrapper">
         <div class="logo-wrapper">
             <img src="<?php echo get_template_directory_uri(); ?>/img/auge.svg" with="100%">
         </div>
+
+        <div class="kasten-name">
+        </div>
+
+        <div class="kasten-theorie">
+        </div>
+
+        <div class="kasten-praxis">
+        </div>
         
-        <div class="kasten-logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/kasten-logo.svg" with="100%">
+        <div class="kopf-modul">
         </div>
         <article>
-
-            <h2>Überschrift</h2>
             <p>Hier steht Text.</p>
             <p>
-                Es gibt im Moment in diese Mannschaft, oh, einige Spieler vergessen ihnen Profi was sie sind. Ich lese nicht sehr viele Zeitungen, aber ich habe gehört viele Situationen. Erstens: wir haben nicht offensiv gespielt. Es gibt keine deutsche Mannschaft spielt offensiv und die Name offensiv wie Bayern. Letzte Spiel hatten wir in Platz drei Spitzen: Elber, Jancka und dann Zickler. Wir 
-                </p>
+                Es gibt im Moment in diese Mannschaft, oh, einige Spieler vergessen ihnen Profi was sie sind. Ich lese nicht sehr viele Zeitungen, aber ich habe gehört viele Situationen. Erstens: wir haben nicht offensiv gespielt. Es gibt keine deutsche Mannschaft spielt offensiv und die Name offensiv wie Bayern. Letzte Spiel hatten wir in Platz drei Spitzen: Elber, Jancka und dann Zickler. Wir
+            </p>
         </article>
     </div>
     <div class="container below-content"></div>
-
-</body>
-    <?php get_footer(); ?>
+</main>
+<?php get_footer(); ?>
